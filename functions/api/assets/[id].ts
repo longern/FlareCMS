c740f0bb7ab0cb03d3b96acd3e68ee05ce0ac6bf
longern <i@longern.com>
2023-11-23 11:53:47 +0800

    Partial content

--- functions/api/assets/[id].ts
+++ functions/api/assets/[id].ts
@@ -3,15 +3,28 @@ interface Env {
 }
 
 export const onRequestGet: PagesFunction<Env> = async (context) => {
-  const { env, params } = context;
+  const { request, env, params } = context;
   const key = params.id as string;
-  const object = await env.BUCKET.get(key);
+
+  const object = await env.BUCKET.get(key, {
+    range: request.headers,
+  }) as R2ObjectBody
   if (!object) {
     return Response.json({ error: "Not found" }, { status: 404 });
   }
 
   const headers = new Headers();
   object.writeHttpMetadata(headers);
-  headers.set("catch-control", "public, max-age=31536000, immutable");
-  return new Response(object.body, { headers });
+  headers.set("catch-control", "public, max-age=31536000");
+  if (object.range) {
+    const range = object.range as {
+      offset: number;
+      length: number;
+      end: number;
+    };
+    const end = range.end ?? object.size - 1;
+    headers.set("content-range", `bytes ${range.offset}-${end}/${object.size}`);
+  }
+  const status = request.headers.get("range") !== null ? 206 : 200;
+  return new Response(object.body, { headers, status });
 };
