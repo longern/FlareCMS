df4ecb00487f402c2dc5f1aaabc35f7afb47796d
longern <i@longern.com>
2023-12-01 14:20:06 +0800

    Get labels list

---
+++ functions/api/labels/index.ts
@@ -0,0 +1,37 @@
+import { sql } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/d1";
+import { labels } from "../schema";
+
+interface Env {
+  DB: D1Database;
+}
+
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+  const { request, env } = context;
+
+  const cache = (caches as CacheStorage & { default: Cache }).default;
+  const cachedResponse = await cache.match(request);
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+
+  const db = drizzle(env.DB);
+  const items = await db
+    .select({ name: labels.name, count: sql<number>`COUNT(*)` })
+    .from(labels)
+    .groupBy(labels.name)
+    .all();
+  items.reverse();
+
+  const response = Response.json(
+    { items },
+    {
+      headers: {
+        "Cache-Control": "public, max-age=3600",
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  cache.put(request, response.clone());
+  return response;
+};
